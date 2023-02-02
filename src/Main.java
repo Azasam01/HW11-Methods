f722ca15c4692ed488e7cbc7a57d5d4d4ca13279
azasa <azasam01@gmail.com>
2023-02-02 14:20:08 +0300

    Homework change

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,11 @@ public class Main {
         task3();
     }
 
-    public static void checkYear (int year){
-        if ((year % 100 == 0) && (year % 400==0) && (year % 4==0)){
-            System.out.println( year + " is a leap year");
+    public static void checkYear(int year) {
+        if ((year % 100 == 0) && (year % 400 == 0) && (year % 4 == 0)) {
+            System.out.println(year + " is a leap year");
         } else {
-            System.out.println( year + " isn't leap year");
+            System.out.println(year + " isn't leap year");
         }
     }
 
@@ -20,46 +20,53 @@ public class Main {
     public static void task1() {
         System.out.println("Задача 1");
         checkYear(2021);
-
     }
 
-    public static void iosOrAndroid (int client, int clientDeviceYear) {
+    public static void iosOrAndroid(int client, int clientDeviceYear) {
 
         int currentYear = LocalDate.now().getYear();
 
         if (client == 0 && clientDeviceYear == currentYear) {
             System.out.println("Install the iOS app version by following the link");
-        } else if (client == 1 && clientDeviceYear == currentYear ) {
+        } else if (client == 1 && clientDeviceYear == currentYear) {
             System.out.println("Install the Android app version by following the link");
         } else if (client == 0 && clientDeviceYear <= currentYear) {
             System.out.println("Install the updated version of the iOS app by following the link");
-        } else if (client == 1 && clientDeviceYear <= currentYear ) {
+        } else if (client == 1 && clientDeviceYear <= currentYear) {
             System.out.println("Install the updated version of the Android app by following the link");
         }
     }
+
     public static void task2() {
         System.out.println("Задача 2");
         iosOrAndroid(1, 2023);
     }
 
-    public static int deliveryTime (int deliveryDistance) {
+    public static int deliveryTime(int deliveryDistance) {
         int day = 1;
-        if (deliveryDistance < 20 ){
-            System.out.println("Delivery within " + deliveryDistance + " km takes " + day + " day");
-        } else if (deliveryDistance >=20 && deliveryDistance < 60) {
-            System.out.println("Delivery within " + deliveryDistance + " km takes " + (1+day) + " days");
-        } else if (deliveryDistance >=60 && deliveryDistance < 100) {
-            System.out.println("Delivery within " + deliveryDistance + " km takes " + (2+day) + " days");
-        } else if (deliveryDistance >=100) {
-            System.out.println("Delivery isn't available");
 
+        if (deliveryDistance < 20) {
+            System.out.println("Delivery within " + deliveryDistance + " km takes " + day + " day");
+
+        } else if (deliveryDistance > 20 && deliveryDistance < 60) {
+            day++;
+            System.out.println("Delivery within " + deliveryDistance + " km takes " + day + " days");
+        } else if (deliveryDistance >= 60 && deliveryDistance < 100) {
+            day += 2;
+            System.out.println("Delivery within " + deliveryDistance + " km takes " + day + " days");
+        } else if (deliveryDistance > 100) {
+            System.out.println("Delivery isn't available");
+            return (-1);
         }
-        return day;
+        return  day;
     }
+
+
 
     public static void task3() {
-        System.out.println("Задача 3");
-        deliveryTime(95);
+            System.out.println("Задача 3");
+            deliveryTime(75);
+        }
     }
 
-}
+
